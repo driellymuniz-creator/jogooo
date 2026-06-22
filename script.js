13dc5a9561b9c6a1b52c77382f43a5c608c0a6cc
driellymuniz-creator <drielly.muniz@aluno.edu.pi.gov.br>
2026-06-22 11:38:25 -0300

    Refactor event listeners and game logic in script.js

--- script.js
+++ script.js
@@ -1,15 +1,65 @@
-let jogador="X";
+let jogador = "X";
 
-document.querySelectorAll(".casa").forEach(casa=>{
+const casas = document.querySelectorAll(".casa");
 
-casa.addEventListener("click",()=>{
+const combinacoesVitoria = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
-if(casa.innerHTML===""){
-casa.innerHTML=jogador;
+casas.forEach(casa => {
 
-jogador=jogador==="X"?"O":"X";
+    casa.addEventListener("click", () => {
+
+        if(casa.innerHTML === ""){
+
+            casa.innerHTML = jogador;
+
+            if(verificarVitoria()){
+                setTimeout(() => {
+                    alert("Jogador " + jogador + " venceu!");
+                }, 100);
+                return;
+            }
+
+            if(verificarEmpate()){
+                setTimeout(() => {
+                    alert("Empate!");
+                }, 100);
+                return;
+            }
+
+            jogador = jogador === "X" ? "O" : "X";
+        }
+
+    });
+
+});
+
+function verificarVitoria(){
+
+    return combinacoesVitoria.some(combinacao => {
+
+        const [a,b,c] = combinacao;
+
+        return (
+            casas[a].innerHTML &&
+            casas[a].innerHTML === casas[b].innerHTML &&
+            casas[a].innerHTML === casas[c].innerHTML
+        );
+
+    });
+
 }
 
-});
+function verificarEmpate(){
 
-});
+    return [...casas].every(casa => casa.innerHTML !== "");
+
+}

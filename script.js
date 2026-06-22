6d83ab3c3f6e3b687657585012b0282f7af89855
driellymuniz-creator <drielly.muniz@aluno.edu.pi.gov.br>
2026-06-22 11:41:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,56 @@
-let jogador = "X";
+let jogadorAtual = "X";
 
 const casas = document.querySelectorAll(".casa");
 
 const combinacoesVitoria = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ];
 
 casas.forEach(casa => {
-
-    casa.addEventListener("click", () => {
-
-        if(casa.innerHTML === ""){
-
-            casa.innerHTML = jogador;
-
-            if(verificarVitoria()){
-                setTimeout(() => {
-                    alert("Jogador " + jogador + " venceu!");
-                }, 100);
-                return;
-            }
-
-            if(verificarEmpate()){
-                setTimeout(() => {
-                    alert("Empate!");
-                }, 100);
-                return;
-            }
-
-            jogador = jogador === "X" ? "O" : "X";
-        }
-
-    });
-
+    casa.addEventListener("click", jogar);
 });
 
-function verificarVitoria(){
+function jogar(event) {
+    const casa = event.target;
 
-    return combinacoesVitoria.some(combinacao => {
+    if (casa.textContent !== "") return;
 
-        const [a,b,c] = combinacao;
+    casa.textContent = jogadorAtual;
 
-        return (
-            casas[a].innerHTML &&
-            casas[a].innerHTML === casas[b].innerHTML &&
-            casas[a].innerHTML === casas[c].innerHTML
-        );
+    if (verificarVitoria()) {
+        alert(`Jogador ${jogadorAtual} venceu!`);
+        return;
+    }
 
-    });
+    if (verificarEmpate()) {
+        alert("Empate!");
+        return;
+    }
 
+    trocarJogador();
 }
 
-function verificarEmpate(){
+function trocarJogador() {
+    jogadorAtual = jogadorAtual === "X" ? "O" : "X";
+}
 
-    return [...casas].every(casa => casa.innerHTML !== "");
+function verificarVitoria() {
+    return combinacoesVitoria.some(([a, b, c]) => {
+        return (
+            casas[a].textContent &&
+            casas[a].textContent === casas[b].textContent &&
+            casas[a].textContent === casas[c].textContent
+        );
+    });
+}
 
+function verificarEmpate() {
+    return [...casas].every(casa => casa.textContent !== "");
 }
